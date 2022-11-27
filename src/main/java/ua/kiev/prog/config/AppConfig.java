4b2bc62d779e80c9ee41ad8283cbd2c79abfc18a
vishchak <denis.vishchak@gmail.com>
2022-11-28 10:10:20 +1100

    initial commit;

---
+++ src/main/java/ua/kiev/prog/config/AppConfig.java
@@ -0,0 +1,108 @@
+package ua.kiev.prog.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Properties;
+
+// db -> E -> DOA/Repos -> S -> C <- Requests
+
+@Configuration
+@ComponentScan("ua.kiev.prog")
+@EnableTransactionManagement
+@PropertySource("classpath:database.properties")
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+    @Value("${db.driver}")
+    private String dbDriver;
+
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.user}")
+    private String dbUser;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory
+            (DataSource dataSource, JpaVendorAdapter jpaVendorAdapter) {
+        Properties jpaProp = new Properties();
+        jpaProp.put("hibernate.hbm2ddl.auto", "create-drop");
+
+        LocalContainerEntityManagerFactoryBean entityManagerFactory =
+                new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter);
+        entityManagerFactory.setJpaProperties(jpaProp);
+        entityManagerFactory.setPackagesToScan("ua.kiev.prog");
+
+        return entityManagerFactory;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+
+        adapter.setShowSql(true);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+
+        return adapter;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+
+        ds.setDriverClassName(dbDriver);
+        ds.setUrl(dbUrl);
+        ds.setUsername(dbUser);
+        ds.setPassword(dbPassword);
+
+        return ds;
+    }
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+
+        resolver.setPrefix("/dynamic/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        resolver.setOrder(1);
+
+        return resolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("/static/");
+    }
+}
---
+++ src/main/java/ua/kiev/prog/config/TestDataBean.java
@@ -0,0 +1,38 @@
+package ua.kiev.prog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import ua.kiev.prog.model.Contact;
+import ua.kiev.prog.model.Group;
+import ua.kiev.prog.services.ContactService;
+import ua.kiev.prog.services.ContactServiceImpl;
+
+import javax.annotation.PostConstruct;
+
+// DB -> JDBC -> H -> JPA -> E1..EN -> DAO/Repo -> S(T) -> V? -> C
+
+@Component
+public class TestDataBean {
+    private final ContactService contactService;
+
+    public TestDataBean(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @PostConstruct
+    public void fillData() {
+        Group group = new Group("Test");
+        Contact contact;
+
+        contactService.addGroup(group);
+
+        for (int i = 0; i < 25; i++) {
+            contact = new Contact(null, "Name" + i, "Surname" + i, "1234567" + i, "user" + i + "@test.com");
+            contactService.addContact(contact);
+        }
+        for (int i = 0; i < 12; i++) {
+            contact = new Contact(group, "Other" + i, "OtherSurname" + i, "7654321" + i, "user" + i + "@other.com");
+            contactService.addContact(contact);
+        }
+    }
+}
